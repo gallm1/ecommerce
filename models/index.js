4b0dce4f4084993b6570de163c821901feb6b083
Michael Galloway <michael.galloway9@gmail.com>
2021-05-10 02:32:00 -0500

    fixed models/index.js

--- models/index.js
+++ models/index.js
@@ -19,16 +19,23 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id'
-})
+  through: {
+    model: ProductTag,
+    unique: false
+},
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id'
-})
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+    as: 'tag_products'
+});
+
 module.exports = {
   Product,
   Category,
